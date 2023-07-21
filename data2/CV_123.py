f617dfd17b098656dfa8b8a556ee2017d689ab12
sunnybabys <88193644+sunnybabys@users.noreply.github.com>
2023-07-21 20:20:04 +0800

    Update CV_123.py

--- data2/CV_123.py
+++ data2/CV_123.py
@@ -18,7 +18,7 @@ class CV():
 
     def bance(self, index, type="train"):
         if self.cv == 1:  # 行
-            t = self.inc_matrix.loc[index]  # 取出inc_matrix中对应index的行（即训练集所对应的行，在相互作用矩阵中）
+            t = self.inc_matrix.loc[index] 
         elif self.cv == 2:  # 列
             t = self.inc_matrix.loc[:, index]
         elif self.cv == 3:  # hl
@@ -27,7 +27,7 @@ class CV():
 
         if self.cv == 1 or self.cv == 2:
             inc = t.stack().reset_index()
-        # 分别获取值为1，0的索引
+       
 
         if(type=="train"):
 
@@ -49,14 +49,14 @@ class CV():
             s = np.vstack((s1, s0))
 
         # print(len(s1),len(s0),len(s))
-        return s  # 返回[[行号，列号]]的二维ndarry
+        return s  
 
     def cv_1(self):
         # 行
         print('cv1 {}行'.format(self.i))
         lens = self.i
         rkf = RepeatedKFold(n_splits=5, n_repeats=self.n_repeats)
-        for train_index, test_index in rkf.split(list(range(lens))):  # 将列表[0,1...lens-1]分割为5分，即按行索引分5份
+        for train_index, test_index in rkf.split(list(range(lens))): 
             self.trains.append(self.bance(train_index,"train"))
             self.tests.append(self.bance(test_index,"test"))
         return self.trains, self.tests
@@ -85,7 +85,7 @@ class CV():
     def get_cv(cls, cv, n_repeats, inc_matrix):
 
         if cv == 1:
-            trains, tests = cls(cv, n_repeats, inc_matrix).cv_1()  # 初始化CV类，即上面的class CV()
+            trains, tests = cls(cv, n_repeats, inc_matrix).cv_1()  
         elif cv == 2:
             trains, tests = cls(cv, n_repeats, inc_matrix).cv_2()
         elif cv == 3:
